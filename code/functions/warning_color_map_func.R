f1d8f9ff7fa9393fde89a67b947b3d4f0158aca4
Luke Vawter <lvawter@icprb.org>
2018-11-19 09:26:24 -0500

    resolving conflict

---
+++ code/functions/warning_color_map_func.R
@@ -0,0 +1,8 @@
+warning_color_map_func <- function(number){
+  case_when(
+    number == 0 ~ map_green,
+    number == 1 ~ map_yellow,
+    number == 2 ~ map_orange,
+    number == 3 ~ map_red
+    
+  )}
